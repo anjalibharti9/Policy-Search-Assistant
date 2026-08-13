220eb236eff58ada59dcbf0dcdfed0ddef3964e4
Anjali Bharti <anjali9.thakur@gmail.com>
2026-08-14 00:15:55 +0530

    Remove ChromaDB test scratch file after switching to FAISS

--- test_chroma.py
+++
@@ -1,5 +0,0 @@
-import chromadb 
-chroma_client = chromadb.PersistentClient(path="./chroma_db") 
-collection = chroma_client.get_or_create_collection(name="policy_docs") 
-print(collection.name) 
-print(collection.count())
